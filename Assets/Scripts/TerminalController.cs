8db0231845336e0cebb6ca57ca3e7416598085b9
acpirate <3apx3zss29@snkmail.com>
2016-12-27 15:12:57 -0500

    toggle console on and off

--- Assets/Scripts/TerminalController.cs
+++ Assets/Scripts/TerminalController.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 
 public class TerminalController : MonoBehaviour {
 
+    public static bool deactivating = false;
+
     public Text terminalDisplay;
     public InputField terminalInput;
 
@@ -16,13 +18,39 @@ public class TerminalController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleConsole();
+        }		
+        
 	}
 
+    void LateUpdate()
+    {
+        deactivating = false;
+    }
+
+    //execute the console command
     public void ProcessCommand()
     {
-        AddLineToTerminalDisplay(terminalInput.text);
-        ResetInput();
+        // dont do it if the terminal isn't active
+       if (!deactivating)
+        {
+            AddLineToTerminalDisplay(terminalInput.text);
+            //empty the input field
+            ResetInput();
+        }
+    }
+
+
+    //turn the console on and off when tab is hit
+    void ToggleConsole()
+    {
+        deactivating = terminalInput.gameObject.activeInHierarchy;
+        terminalInput.gameObject.SetActive(!terminalInput.gameObject.activeInHierarchy);
+        terminalDisplay.gameObject.transform.parent.parent.gameObject.SetActive(!terminalDisplay.gameObject.transform.parent.parent.gameObject.activeInHierarchy);
+
+        if (terminalInput.gameObject.activeInHierarchy) terminalInput.ActivateInputField();
     }
 
     void ResetInput()
