4334a069a29dfaac6f728c7e0581b0a79070078c
acpirate <3apx3zss29@snkmail.com>
2017-03-15 17:09:21 -0400

    background music

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -9,8 +9,10 @@ public class CreditsController : MonoBehaviour {
     public TextMeshProUGUI creditsTextDisplay;
 
 	// Use this for initialization
-	void Start () {
-      //  Debug.Log(((TextAsset)Resources.Load("Credits")).text);
+	void Start ()
+    {
+        MusicController.instance.PlayCreditsMusic();
+        //  Debug.Log(((TextAsset)Resources.Load("Credits")).text);
         creditsTextDisplay.text = ((TextAsset)Resources.Load("TextFiles/Credits")).text;
     }
 	
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour {
+
+    public static MusicController instance;
+
+    public AudioClip titleMusic;
+    public AudioClip selectMusic;
+    public AudioClip gameplayMusic;
+    public AudioClip creditsMusic;
+    public AudioClip gameoverMusic;
+
+    AudioSource mySource;
+
+    void Awake()
+    {
+        //singleton pattern
+        DontDestroyOnLoad(gameObject);
+        if (instance == null) {
+            instance = this;
+            }
+        else Destroy(gameObject);
+
+        mySource = GetComponentInChildren<AudioSource>();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.activeSceneChanged += SceneChange;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= SceneChange;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void PlayTitleMusic()
+    {
+        if (mySource.isPlaying) mySource.Stop();
+
+        mySource.clip = titleMusic;
+        mySource.Play();
+    }
+
+    public void PlaySelectMusic()
+    {
+        if (mySource.isPlaying) mySource.Stop();
+
+        mySource.clip = selectMusic;
+        mySource.Play();
+    }
+
+    public void PlayGameplayMusic()
+    {
+        if (mySource.isPlaying) mySource.Stop();
+
+        mySource.clip = gameplayMusic;
+        mySource.Play();
+    }
+
+    public void PlayGameoverMusic()
+    {
+        if (mySource.isPlaying) mySource.Stop();
+
+        mySource.clip = gameoverMusic;
+        mySource.Play();
+    }
+    
+    public void PlayCreditsMusic()
+    {
+        if (mySource.isPlaying) mySource.Stop();
+
+        mySource.clip = creditsMusic;
+        mySource.Play();
+    }
+
+    void SceneChange(Scene scene1, Scene scene2)
+    {
+        if (scene2.name=="Title")
+        {
+            PlayTitleMusic();
+        }
+        if (scene2.name=="Game")
+        {
+            PlayGameplayMusic();
+        }
+        if (scene2.name=="GameOver")
+        {
+            PlayGameoverMusic();
+        }
+        if (scene2.name=="Credits")
+        {
+            PlayCreditsMusic();
+        }
+        if (scene2.name=="ModeSelect")
+        {
+            PlaySelectMusic();
+        }
+    }
+}
